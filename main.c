d8048b91c5b31ee2f5d7ebb5d8e924a07229ddf2
kevin <fernandezkevin@hotmail.com.ar>
2018-03-22 20:59:41 -0300

    primer commit

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numeroUno;
+    int numeroDos;
+    int resultado;
+    char respuesta;
+
+    while(respuesta!='s')
+    {
+        printf("ingresar un numero\n");
+        scanf("%d",&numeroUno);
+        printf("ingrese segundo numero\n");
+        scanf("%d",&numeroDos);
+
+        if(numeroUno>0 && numeroDos>0)
+        {
+            resultado=numeroUno+numeroDos;
+            printf("el resultado es: %d \n",resultado);
+        }
+        else
+        {
+            printf("No se puede realizar la operacion");
+        }
+
+        printf("Para terminar ingrese la letra s \n");
+        fflush(stdin);
+        scanf("%c",&respuesta);
+
+
+    }
+    return 0;
+}
